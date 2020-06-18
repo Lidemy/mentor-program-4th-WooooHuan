19aaa62715c94fcb873129e01c6526db78243614
Woo <WooooHuan@gmail.com>
2020-06-19 02:19:20 +0800

    fix hw5, hw6 done

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -6,6 +6,13 @@ function join(arr, concatStr) {
     return tmp;
 }
 
+// 宣告空字串 tmp 作為暫存用途
+// 宣告 last 來暫存從 arr pop() 出來的資料
+// 替 tmp 進行遞迴累加，製作字串夾心
+// 為了製造夾心狀態，補上先前 pop 出來的資資料
+// 回傳 tmp
+
+
 function repeat(str, times) {
     let tmp = str;
     for (let i = 1; i < times; i++) {
@@ -13,6 +20,12 @@ function repeat(str, times) {
     }
     return tmp;
 }
+
+// 宣告空字串 tmp 作為 str 的副本
+// 宣告 last 來暫存從 arr pop() 出來的資料
+// 替 tmp 進行遞迴累加 str 的內容
+// 回傳 tmp
+
 
 console.log(join(['a', 'b', 'c'], '!'));
 console.log(repeat('a', 5));
