6487015cb401c2091decc756d5649dc88ef193ad
wuhuan <wuhuan@rayark.com>
2020-06-29 21:14:29 +0800

    hw3 done

---
+++ homeworks/week3/hw3.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin });
+const lines = [];
+
+function solve(n) {
+  if (n > 2) {
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  } return n === 2;
+}
+
+function input(inputInfo) {
+  const info = inputInfo.map(x => parseInt(x, 10));
+  for (let i = 1; i <= info[0]; i += 1) {
+    console.log(solve(info[i]) ? 'Prime' : 'Composite');
+  }
+}
+
+rl.on('line', line => lines.push(line));
+rl.on('close', () => input(lines));
