81fd008b83b53e423eb0799829bc1590501513b6
wuhuan <wuhuan@rayark.com>
2020-07-06 22:58:02 +0800

    w4 practice

---
+++ practice/net101/HttpApi.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const process = require('process');
+
+/* request(
+  'https://reqres.in/api/users',
+  (error , response, body) => console.log(body)
+);
+
+request(
+  'https://reqres.in/api/users/2',
+  (error , response, body) => console.log(body)
+); */
+
+request(
+  `https://reqres.in/api/users/${process.argv[2]}`,
+  (error, response, body) => console.log(body),
+);
