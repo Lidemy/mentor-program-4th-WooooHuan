7736950d61c2f6dae32ce4dde50c10f9cc1960a6
wuhuan <wuhuan@rayark.com>
2020-07-08 18:19:24 +0800

    hw2 done

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,9 +1,7 @@
 const request = require('request');
 
-const baseUrl = 'https://lidemy-book-store.herokuapp.com';
-
 request(
-  `${baseUrl}/books?_limit=10`,
+  'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
     const books = JSON.parse(body);
     books.forEach(book => console.log(`${book.id} ${book.name}`));
