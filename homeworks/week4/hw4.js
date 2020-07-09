366abd89ef0f06ef5056a1bbb00b32d66e4709c6
wuhuan <wuhuan@rayark.com>
2020-07-09 22:20:12 +0800

    hw4 done

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+function main() {
+  request({
+    method: 'GET',
+    url: 'https://api.twitch.tv/kraken/games/top',
+    headers: {
+      'Client-ID': 'jl04gehwmgpr795vsgeajymfb4bk95',
+      Accept: 'application/vnd.twitchtv.v5+json',
+    },
+  },
+  (error, response, body) => {
+    if (error) { console.log('Request error!'); return; }
+    const infoList = JSON.parse(body).top;
+    infoList.forEach(e => console.log(`${e.viewers} ${e.game.name}`));
+  });
+}
+
+main();
