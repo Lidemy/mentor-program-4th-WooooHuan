0ffb97949f5b7f1c615a9d3b370f2687fc86a30e
wuhuan <wuhuan@rayark.com>
2020-08-12 17:17:45 +0800

    hw1 done

--- homeworks/week7/hw1/script.js
+++ homeworks/week7/hw1/script.js
@@ -1,65 +1,37 @@
-const nickNameText = "nickname-text";
-const emailText = "email-text";
-const phoneText = "phone-text";
-const signUpType = "sign-up-type";
-const knowsText = "knows-text";
-const extraText = "extra-text";
-const necessaryHint = "這是必填問題！";
-
-document.querySelector("button").addEventListener("click", onClicked);
+document.querySelector(".submit-btn").addEventListener("click", onClicked);
 
 function onClicked() {
   const inputList = document.querySelectorAll(".content-input-group")
   const result = [];
+  let legal = true;
   for (input of inputList) {
-    const title = input.getElementFromChild(input, "input-title", true);
-    const inputElement = getElementFromChild(input, "input", false);
-    const inputInfo = getInputInfo(inputElement);
+    const title = getTitle(getElementFromChildByClass(input, "input-title"));
+    const inputTextElement = getElementFromChildByClass(input, "input-text");
+    const inputInfo = getInputInfo(inputTextElement);
+    const inputHint =  getElementFromChildByClass(input, "input-hint");
+    if (inputHint) inputHint.innerText = inputInfo.length > 0 ? "" : "這是必填問題！";
     result.push(`${title}：${inputInfo}`);
+    legal = legal && inputInfo.length > 0;
+  } if (legal) alert(result.join("\n"));
+}
+
+function getElementFromChildByClass (element, className) {
+  for (node of element.childNodes) {
+    if (node.className === className) return node;
   }
 }
 
-function getElementFromChild (element, name, byClassName) {
-  for (node in element.childNodes) {
-    if (byClassName ? node.className : node.tagName === name) return node;
-  }
+function getTitle (element) {
+  return element.innerText.split(" ")[0];
 }
 
-function Temp () {
-  const textElements = document.querySelectorAll(".input-text");
-  for (element of textElements) {
-    if (element.value.length > 0) {
-
-    } else {
-      showNecessaryHint(element);
+function getInputInfo (inputTextElement) {
+  if (inputTextElement) {
+    return inputTextElement.value;
+  } else {
+    const radioList = document.querySelectorAll(".input-radio");
+    for (radio of radioList) {
+      if (radio.checked) return radio.parentNode.innerText.trim();
     }
-  }
-}
-
-function getInputText (name) {
-  const inputTextList = document.querySelectorAll(".input-text");
-  for (inputText of inputTextList) {
-    if (inputText.name === name) return inputText.value;
-  }
-}
-
-function getInputInfo (inputElement) {
-  switch (inputElement.type) {
-    case "text" :
-      return getElementFromChild(inputElement, "input-text", true).value;
-    case "radio" : 
-      const radioList = document.querySelectorAll(".input-radio");
-      for (radio of radioList) {
-        if (radio.checked) return
-      }
-    default : console.log("somthing bad happened :(");
-  }
-}
-
-
-function showNecessaryHint (element) {
-  const nodes = element.parentNode.childNodes;
-  for (node of nodes) {
-    if (node.className === "input-hint") node.innerText = necessaryHint;
   }
 }
