5cae9a97047344bce481a7bd15703a36f0750f2a
wuhuan <wuhuan@rayark.com>
2020-08-12 20:44:58 +0800

    polish

--- homeworks/week7/hw1/script.js
+++ homeworks/week7/hw1/script.js
@@ -1,36 +1,44 @@
-function getElementFromChildByClass(element, className) {
+function getChildElem(element, className) {
   for (const node of element.childNodes) {
     if (node.className === className) return node;
   }
 }
 
 function getTitle(element) {
-  return element.innerText.split(' ')[0];
+  return getChildElem(element, 'input-title').innerText.split(' ')[0];
 }
 
-function getInputInfo(inputTextElement) {
-  if (inputTextElement) {
-    return inputTextElement.value;
-  }
+function getRadioInfo() {
   const radioList = document.querySelectorAll('.input-radio');
   for (const radio of radioList) {
     if (radio.checked) return radio.parentNode.innerText.trim();
   }
 }
 
-function onClicked() {
+function getInputInfo(element) {
+  const inputTextElement = getChildElem(element, 'input-text');
+  return inputTextElement ? inputTextElement.value : getRadioInfo();
+}
+
+function isLegal(element) {
+  const legal = getInputInfo(element).length > 0;
+  const inputHint = getChildElem(element, 'input-hint');
+  if (inputHint) {
+    const hintIcon = getChildElem(inputHint, 'hint-icon');
+    const hintText = getChildElem(inputHint, 'hint-text');
+    hintIcon.style.display = legal ? 'none' : 'inline-block';
+    hintText.innerText = legal ? '' : '這是必填問題';
+    return legal;
+  } return true;
+}
+
+document.querySelector('.submit-btn').addEventListener('click', () => {
   const inputList = document.querySelectorAll('.content-input-group');
   const result = [];
   let legal = true;
   for (const input of inputList) {
-    const title = getTitle(getElementFromChildByClass(input, 'input-title'));
-    const inputTextElement = getElementFromChildByClass(input, 'input-text');
-    const inputInfo = getInputInfo(inputTextElement);
-    const inputHint = getElementFromChildByClass(input, 'input-hint');
-    if (inputHint) inputHint.innerText = inputInfo.length > 0 ? '' : '這是必填問題！';
-    result.push(`${title}：${inputInfo}`);
-    legal = legal && inputInfo.length > 0;
-  } if (legal) alert(result.join('\n'));
-}
-
-document.querySelector('.submit-btn').addEventListener('click', onClicked);
+    legal = isLegal(input) && legal;
+    result.push(`${getTitle(input)}：${getInputInfo(input)}`);
+  }
+  if (legal) alert(result.join('\n'));
+});
