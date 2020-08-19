c64999360790c77a9cb255cc5f1e9e9214a29a47
Woo <woooohuan@gmail.com>
2020-08-20 02:23:26 +0800

    hw2 wip

--- homeworks/week8/hw2/script.js
+++ homeworks/week8/hw2/script.js
@@ -1,30 +1,49 @@
 /* eslint-disable */
 const tabsRoot = document.querySelector('.tabs-bar');
 const tabTemplate = document.querySelector('.tab-template');
+const streamRoot = document.querySelector('.tab-content');
 const streamTamplate = document.querySelector('.stream-template');
-const tabBtns = document.body.getElementsByClassName('tab-btn'); //可省
-let lastPatchNode = getElement(tabBtns[0], 'tab-patch');
 const apiUrl = 'https://api.twitch.tv/kraken';
 const errMsg = '系統不穩定，請再試一次';
+let streamList;
+let lastPatchNode;
 
 function getElement(parentNode, className) {
   return parentNode.getElementsByClassName(className)[0];
 }
 
-function inRange(x, min, max) {
-  return ((x - min) * (x - max) <= 0);
-}
-
 function showError() {
   alert(errMsg);
 }
-/*
+
+function updateStreams(data) {
+
+}
+
+function initStreams(data) {
+  for (let i = 0; i < 20; i++) {
+    const clone = tabTemplate.cloneNode(true);
+    clone.classList.remove('tab-template');
+    tabsRoot.appendChild(clone);
+    if (i === 0) {
+      lastPatchNode = getElement(clone, 'tab-patch');
+      lastPatchNode.classList.remove('hidden');
+    }
+  }
+}
+
 function initTabs(data) {
-
-}*/
-
-function showStreams(data) {
-  console.log(data);
+  for (let i = 0; i < 5; i++) {
+    const name = data.top[i].game.name;
+    const clone = tabTemplate.cloneNode(true);
+    clone.classList.remove('tab-template');
+    getElement(clone, 'tab-title').innerText = name;
+    tabsRoot.appendChild(clone);
+    if (i === 0) {
+      lastPatchNode = getElement(clone, 'tab-patch');
+      lastPatchNode.classList.remove('hidden');
+    }
+  }
 }
 
 function sendRequest(req, reqUrl, cb) {
@@ -33,14 +52,11 @@ function sendRequest(req, reqUrl, cb) {
   req.setRequestHeader('Client-ID', 'jl04gehwmgpr795vsgeajymfb4bk95');
   req.send();
   req.onload = () => {
-    if (inRange(req.status, 200, 399)) {
+    if ((req.status - 200) * (req.status - 399) <= 0) {
       let data;
-      try {
-        data = JSON.parse(req.response);
-      } catch (err) {
-        showError();
-        return;
-      } cb(data);
+      try { data = JSON.parse(req.response); }
+      catch (err) { showError(); return; } 
+      cb(data);
     } else {
       showError();
     }
@@ -49,56 +65,40 @@ function sendRequest(req, reqUrl, cb) {
 
 function getStreams(name) {
   const req = new XMLHttpRequest();
-  const reqUrl = `${apiUrl}/streams?game=${encodeURIComponent(name)}&limit=20`;
-  sendRequest(req, reqUrl, showStreams);
+  const reqUrl = `${apiUrl}/streams?game=${encodeURIComponent(name)}`;
+  sendRequest(req, reqUrl, initStreams);
 }
 
-function initTabs(data) {
-  for (let i = 0; i < 5; i++) {
-    const name = data.top[i].game.name;
-    const clone = tabTemplate.cloneNode(true);
-    clone.value = name;
-    clone.classList.remove('tab-template');
-    getElement(clone, 'tab-title').innerText = name;
-    tabsRoot.appendChild(clone);
-    if (i > 0) getElement(clone, 'tab-patch').classList.add('hidden'); // 處理 tab 相關
-  } 
+function updateTab(newTabElem) {
+  const newPatchNode = getElement(newTabElem, 'tab-patch');
+  lastPatchNode.classList.toggle('hidden');
+  newPatchNode.classList.toggle('hidden');
+  lastPatchNode = newPatchNode;
 }
 
-function getGamesInfo() {
+function tabBtn(element) {
+  updateTab(element);
+  getStreams(getElement(element, 'tab-title').innerText);
+}
+
+document.body.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (!e.target.value) return;
+  const fn = window[e.target.value];
+  if (typeof fn === 'function') fn(e.target);
+});
+
+window.onload = () => {
   const req = new XMLHttpRequest();
-  const reqUrl = `${apiUrl}/games/top?limit=10`;
+  const reqUrl = `${apiUrl}/games/top`;
   sendRequest(req, reqUrl, initTabs);
 }
 
-getGamesInfo();
+///////////////////
+
 /*function setNewTask() {
   const clone = taskTemplate.cloneNode(true);
   clone.classList.remove('task-template');
   getElement(clone, 'task-text').innerText = getNewTaskTitle();
   listMainNode.appendChild(clone);
 }*/
-
-function tabBtn(element, n) {
-  const patchNode = getElement(element, 'tab-patch');
-  patchNode.classList.toggle('hidden');
-  lastPatchNode.classList.toggle('hidden');
-  lastPatchNode = patchNode;
-}
-
-tabsRoot.addEventListener('click', (e) => {
-  if (!e.target.value) return;
-  const targetInfo = e.target.value.split('-');
-  const fn = window[targetInfo[0]];
-  if (typeof fn === 'function') fn(e.target, targetInfo[1]);
-});
-
-///////////
-
-document.querySelector('.games').addEventListener('click', (e) => {
-  getGamesInfo();
-});
-
-document.querySelector('.streams').addEventListener('click', (e) => {
-  getStreams('Just Chatting');
-});
