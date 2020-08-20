8faec9a8589c862ebdecaf0a57016b6f68076cec
wuhuan <wuhuan@rayark.com>
2020-08-20 22:03:01 +0800

    hw2 done

--- homeworks/week8/hw2/script.js
+++ homeworks/week8/hw2/script.js
@@ -5,27 +5,43 @@ const strmRoot = document.querySelector('.tab-content');
 const strmTamplate = document.querySelector('#stream-template');
 const apiUrl = 'https://api.twitch.tv/kraken';
 const errMsg = '系統不穩定，請再試一次';
+let curGame = '';
 let tabList = [];
 let strmList = [];
+let dataList = [];
 let lastPatchNode;
-
-function getElem(parentNode, className) {
-  return parentNode.getElementsByClassName(className)[0];
-}
+let reqCount = 20;
 
 function showError() {
   alert(errMsg);
 }
 
+function resetStrmData() {
+  dataList = [];
+  reqCount = 20;
+  for (strm of strmList) {
+    strm.remove();
+  } strmList = [];
+}
+
+function getElem(parentNode, className) {
+  return parentNode.getElementsByClassName(className)[0];
+}
+
 function initStrms(data) {
-  console.log(data);
-  if (strmList.length === 0) cloneTemplate(strmTamplate, 20, strmRoot, strmList);
-  const strms = data.streams;
-  for (let i = 0; i < strmList.length; i++) {
-    getElem(strmList[i], 'img-preview').src = strms[i].preview.large;
-    getElem(strmList[i], 'img-avatar').src = strms[i].channel.logo;
-    getElem(strmList[i], 'stream-title').innerText = strms[i].channel.status;
-    getElem(strmList[i], 'stream-channel').innerText = strms[i].channel.name;
+  dataList = dataList.concat(data.streams);
+  if (dataList.length < reqCount) {
+    getStrms(curGame);
+    return;
+  }   
+  const lastListLength = strmList.length;
+  cloneTemplate(strmTamplate, reqCount - lastListLength, strmRoot, strmList);
+  for (let i = lastListLength; i < reqCount; i++) {
+    getElem(strmList[i], 'img-preview').src = dataList[i].preview.large;
+    getElem(strmList[i], 'img-avatar').src = dataList[i].channel.logo;
+    getElem(strmList[i], 'stream-title').innerText = dataList[i].channel.status;
+    getElem(strmList[i], 'stream-channel').innerText = `id : ${dataList[i].channel.name}
+      viewers : ${dataList[i].viewers}`;
   }
 }
 
@@ -37,8 +53,10 @@ function initTabs(data) {
     if (i === 0) {
       lastPatchNode = getElem(tabList[i], 'tab-patch');
       lastPatchNode.classList.remove('hidden');
+      curGame = name;
+      getStrms(name);
     }
-  } getStrms(data.top[0].game.name);
+  } 
 }
 
 function cloneTemplate(target, count, root, list) {
@@ -70,11 +88,11 @@ function sendRequest(req, reqUrl, cb) {
 
 function getStrms(name) {
   const req = new XMLHttpRequest();
-  const reqUrl = `${apiUrl}/streams?game=${encodeURIComponent(name)}`;
+  const reqUrl = `${apiUrl}/streams?game=${encodeURIComponent(name)}&limit=100&offset=${dataList.length}`;
   sendRequest(req, reqUrl, initStrms);
 }
 
-function updateTab(newTabElem) {
+function updatePatch(newTabElem) {
   const newPatchNode = getElem(newTabElem, 'tab-patch');
   lastPatchNode.classList.toggle('hidden');
   newPatchNode.classList.toggle('hidden');
@@ -82,8 +100,17 @@ function updateTab(newTabElem) {
 }
 
 function tabBtn(element) {
-  updateTab(element);
-  getStrms(getElem(element, 'tab-title').innerText);
+  const name = getElem(element, 'tab-title').innerText;
+  if (name === curGame) return;
+  updatePatch(element);
+  resetStrmData();
+  curGame = name;
+  getStrms(curGame);
+}
+
+function moreBtn() {
+  reqCount += 20;
+  getStrms(curGame);
 }
 
 document.body.addEventListener('click', (e) => {
@@ -97,3 +124,32 @@ window.onload = () => {
   const reqUrl = `${apiUrl}/games/top`;
   sendRequest(req, reqUrl, initTabs);
 }
+
+/*
+let count = 0;
+let datalist = [];
+let requestCount = 500;
+
+test(0);
+function test(offset) {
+  const name = 'League of Legends';
+  const req = new XMLHttpRequest();
+  const reqUrl = `${apiUrl}/streams?game=${encodeURIComponent(name)}&limit=100&offset=${offset}`;
+
+  req.open('GET', reqUrl, true);
+  req.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+  req.setRequestHeader('Client-ID', 'jl04gehwmgpr795vsgeajymfb4bk95');
+  req.send();
+  req.onload = () => {
+    data = JSON.parse(req.response);
+    datalist = datalist.concat(data.streams);
+    if (datalist.length < requestCount) {
+      test(datalist.length);
+      return;
+    } else {
+      for (let i = 0; i < requestCount; i++) {
+        console.log(i + ' : ' + datalist[i].viewers);
+      }
+    }
+  };
+}*/
