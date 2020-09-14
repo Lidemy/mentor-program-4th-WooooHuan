d0c1941eb750470e198653944b7baa9d7fbcc2bc
Woo <woooohuan@gmail.com>
2020-09-15 02:25:19 +0800

    hw1 wip

--- examples/week12/hw1/api_add_comments.php
+++ examples/week12/hw1/api_add_comments.php
@@ -1,47 +1,50 @@
 <?php
-  require_once('conn.php');
-  header('Content-type:application/json;charset=utf-8');
-  header('Access-Control-Allow-Origin: *');
-  if (
-    empty($_POST['content']) ||
-    empty($_POST['nickname']) ||
-    empty($_POST['site_key'])
-  ) {
-    $json = array(
-      "ok" => false,
-      "message" => "Please input missing fields"
-    );
 
-    $response = json_encode($json);
-    echo $response;
-    die();
-  }
+require_once('conn.php');
+header('Content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 
-  $nickname = $_POST['nickname'];
-  $site_key = $_POST['site_key'];
-  $content = $_POST['content'];
-
-  $sql = "insert into discussions(site_key, nickname, content) values (?, ?, ?)";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param('sss', $site_key, $nickname, $content);
-  $result = $stmt->execute();
-
-  if (!$result) {
-    $json = array(
-      "ok" => false,
-      "message" => $conn->error
-    );
-    $response = json_encode($json);
-    echo $response;
-    die();
-  }
-
+if (
+  empty($_POST['content']) ||
+  empty($_POST['nickname']) ||
+  empty($_POST['site_key'])
+) {
   $json = array(
-    "ok" => true,
-    "message" => "success"
+    "ok" => false,
+    "message" => "Please input content"
   );
 
   $response = json_encode($json);
   echo $response;
-?>
+  die();
+}
 
+$content = $_POST['content'];
+$site_key = $_POST['site_key'];
+$nickname = $_POST['nickname'];
+
+$sql = "INSERT INTO woo_w12_comments(site_key, nickname, content)
+  VALUES (?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('sss', $site_key, $nickname, $content);
+
+$result = $stmt->execute();
+
+if (!$result) {
+  $json = array(
+    "ok" => false,
+    "message" => $conn->error
+  );
+  $response = json_encode($json);
+  echo $response;
+  die();
+}
+
+$json = array(
+  "ok" => true,
+  "message" => "success"
+);
+
+$response = json_encode($json);
+echo $response;
