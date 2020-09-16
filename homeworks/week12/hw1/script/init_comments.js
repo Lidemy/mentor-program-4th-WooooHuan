f5dc2b94a7d8609031808770fa8fdfc32e2dcd10
Woo <woooohuan@gmail.com>
2020-09-17 04:18:01 +0800

    hw1 done

---
+++ homeworks/week12/hw1/script/init_comments.js
@@ -0,0 +1,54 @@
+const root = $('.list-tasks');
+let limit = 5;
+
+function cleanLayout() {
+  $('.input-nickname').val('');
+  $('.input-content').val('');
+  root.empty();
+}
+
+function getComments() {
+  $.ajax({
+    type: "GET",
+    url: `http://localhost/woo/week12/hw1/php/api_get_comments.php?site_key=woo&limit=${limit + 1}`
+  }).done(initComments);
+}
+
+function initComments(data) {
+  cleanLayout();
+  const count = data.length <= limit ? data.length : data.length - 1;
+
+  for (let i = 0; i < count; i++) {
+    const comment = $('.task-template').clone();
+    comment.find('.task-nickname').text(data[i].nickname);
+    comment.find('.task-text').text(data[i].content);
+    comment.find('.del-btn').click(() => deleteComment(data[i]));
+    comment.removeClass('task-template');
+    root.append(comment);
+  }
+  if (data.length <= limit) {
+    $('.more-element').hide();
+  } else {
+    $('.more-element').show();
+  }
+}
+
+function deleteComment(data) {
+  $.ajax({
+    method: "POST",
+    url: "http://localhost/woo/week12/hw1/php/api_del_comment.php",
+    data
+  }).done(getComments);
+}
+
+function initShowMoreBtn() {
+  $('.more-btn').click(() => {
+    limit += 5;
+    getComments();
+  });
+}
+
+initShowMoreBtn();
+getComments();
+
+export { getComments };
--- homeworks/week12/hw1/script/post_comment.js
+++ homeworks/week12/hw1/script/post_comment.js
@@ -1,7 +1,10 @@
-$('.new-task-btn').click(() => {
+import { getComments } from './init_comments.js';
+
+function postComment() {
   const nickname = $('.input-nickname').val();
   const content = $('.input-content').val();
   if (!nickname || !content) return;
+
   $.ajax({
     method: "POST",
     url: "http://localhost/woo/week12/hw1/php/api_post_comment.php",
@@ -10,5 +13,14 @@ $('.new-task-btn').click(() => {
       nickname,
       content
     }
-  }).done(() => location.reload());
-});
+  }).done(getComments);
+}
+
+function onKeyUp(e) {
+  const keyUp = e.key === 'Enter' || e.keyCode === 13;
+  if (keyUp) postComment();
+}
+
+$('.new-task-btn').click(postComment);
+$('.input-nickname').keyup(onKeyUp);
+$('.input-content').keyup(onKeyUp);
