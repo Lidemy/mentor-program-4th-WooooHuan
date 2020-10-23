7174b9921210443853b70005d94349c2eb53036e
Woo <woooohuan@gmail.com>
2020-10-24 02:46:28 +0800

    hw2 wip

--- homeworks/week17/hw2/public/init.js
+++ homeworks/week17/hw2/public/init.js
@@ -1,20 +1,19 @@
 const $rewardRoot = $('.reward-root');
+const $createRoot = $('.tr-create-reward');
 const $secLottery = $('.sec-lottery');
 const $secSettings = $('.sec-settings');
 const $lotteryBtn = $('.lottery-btn');
 const $settingsBtn = $('.settings-btn');
+const $error = $('.error');
 const $doneBtn = $('.done-btn');
 const $createBtn = $('.create-btn');
+let rewards;
 
 $settingsBtn.click(async () => {
   $secLottery.hide();
-  const rewards = JSON.parse(await getRewards());
-  for (let reward of rewards) {
-    const $reward = $(getRewardHtml(reward));
-    const $delBtn = $reward.find('.delete-btn');
-    $delBtn.click(() => $reward.remove);
-    $rewardRoot.append($reward);
-  }
+  showError(false);
+  resetInputValues($createRoot);
+  await renderSettings();
   $secSettings.show();
 });
 
@@ -24,3 +23,32 @@ $doneBtn.click(async () => {
   await updateRewards(settings);
   $secLottery.show();
 });
+
+$createBtn.click(async () => { 
+  const reward = getInputValues($createRoot);
+  const isLegaled = isThatPropsAreExist(reward);
+  if (isLegaled) {
+    appendRewardElement(reward);
+    resetInputValues($createRoot);
+  }
+  showError(!isLegaled);
+});
+
+async function renderSettings() {
+  if (rewards) return;
+  rewards = JSON.parse(await getRewards());
+  for (let reward of rewards) {
+    appendRewardElement(reward);
+  }
+}
+
+function appendRewardElement(reward) {
+  const $reward = $(getRewardHtml(reward));
+  const $delBtn = $reward.find('.delete-btn');
+  $delBtn.click(() => $reward.remove());
+  $rewardRoot.append($reward);
+}
+
+function showError(bool) {
+  $error.css('display', bool ? 'block' : 'none');
+}
--- homeworks/week17/hw2/public/utils.js
+++ homeworks/week17/hw2/public/utils.js
@@ -42,7 +42,7 @@ function getRewardHtml(reward) {
       <input class="input-description" type="text" style="width:360px;" value="${reward.description}">
     </td>
     <td align="center">
-      <input class="input-weight" type="text" style="width:80px;" value="${reward.description}">
+      <input class="input-weight" type="text" style="width:80px;" value="${reward.weight}">
     </td>
     <td align="center">
       <button class="delete-btn">Delete</button>
@@ -55,17 +55,34 @@ function getSettings() {
   const $inputList = $('.reward-template');
   const settings = [];
   for (let input of $inputList) {
-    const $elem = $(input);
-    settings.push({
-      imgUrl: $elem.find('.input-img-url').val(),
-      description: $elem.find('.input-description').val(),
-      weight: parseWeight($elem.find('.input-weight').val()),
-    });
+    settings.push(getInputValues($(input)));
   }
   return JSON.stringify(settings);
+}
+
+function getInputValues($root) {
+  const values = {
+    imgUrl: $root.find('.input-img-url').val(),
+    description: $root.find('.input-description').val(),
+    weight: parseWeight($root.find('.input-weight').val()),
+  };
+  return values;
+}
+
+function resetInputValues($root) {
+  $root.find('.input-img-url').val('');
+  $root.find('.input-description').val('');
+  $root.find('.input-weight').val('');
 }
 
 function parseWeight(value) {
   let weight = parseInt(value, 10);
   return isNaN(weight) ? 1 : Math.max(weight, 1);
+}
+
+function isThatPropsAreExist(obj) {
+  for (let prop in obj) {
+    if (!obj[prop]) return false;
+  } 
+  return true;
 }
